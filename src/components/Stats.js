cdafa237f22efcb892b5825c30b3434c70f8fdff
jacek zablocki <56847035+actionjacks@users.noreply.github.com>
2021-02-01 23:44:18 +0100

    api bug

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,10 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Stats.css";
+import axios from "axios";
 
 function Stats() {
+  const APIKEY = "&token=c0c713748v6u6kubf4lg";
+  const BASE_URL = "https://finnhub.io/api/v1/quote?symbol=";
+
+  const [stockData, setStockData] = useState([]);
+
+  const getStocksData = (stock) => {
+    return axios
+      .get(`${BASE_URL}${stock}${APIKEY}`)
+      .then((item) => {
+        console.log(item);
+      })
+      .catch((error) => {
+        console.error("Error", error.message);
+      });
+  };
+
+  useEffect(() => {
+    const stocksList = [
+      "AAPL",
+      "MSFT",
+      "TSLA",
+      "FB",
+      "BABA",
+      "UBER",
+      "DIS",
+      "SBUX",
+    ];
+
+    let tempStockData = [];
+    let promises = [];
+    stocksList.map((stock) => {
+      promises.push(
+        getStocksData(stock).then((res) => {
+          console.log(res);
+          tempStockData.push({
+            name: stock,
+            ...res.data,
+          });
+        })
+      );
+    });
+    Promise.all(promises).then(() => {
+      setStockData(tempStockData);
+      console.log(tempStockData);
+    });
+  }, []);
+
   return (
     <div className="stats">
-      <p></p>
+      <div className="stats__container">
+        <div className="stats__header">
+          <p>Stocks</p>
+        </div>
+        <div className="stats__content">
+          <div className="stats__rows">{/*  */}</div>
+        </div>
+        <div className="stats__header">
+          <p>Lists</p>
+        </div>
+        <div className="stats__content">
+          <div className="stats__rows">{/*  */}</div>
+        </div>
+      </div>
     </div>
   );
 }
