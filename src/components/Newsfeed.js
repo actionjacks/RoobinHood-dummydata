ecaf3c2de85b419e324fcfeeca5e19a33dfced44
jacek zablocki <56847035+actionjacks@users.noreply.github.com>
2021-02-03 23:26:20 +0100

    add timeLine component

--- src/components/Newsfeed.js
+++ src/components/Newsfeed.js
@@ -1,8 +1,32 @@
 import React from "react";
 import LineGraph from "./LineGraph";
 import "./Newsfeed.css";
+import TimeLine from "./TimeLine";
+import Chip from "@material-ui/core/Chip";
+import { Avatar } from "@material-ui/core";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import FlashOnIcon from "@material-ui/icons/FlashOn";
 
 function Newsfeed() {
+  const [popularTopics, setTopics] = useState([
+    "Technology",
+    "Top Movies",
+    "Upcoming Earnings",
+    "Crypto",
+    "Cannabis",
+    "Healthcare Supplies",
+    "Index ETFs",
+    "Technology",
+    "China",
+    "Pharma",
+  ]);
+
+  const [seed, setSeed] = useState("");
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+
   return (
     <div className="newsfeed">
       <div className="newsfeed__container">
@@ -13,6 +37,37 @@ function Newsfeed() {
           </div>
           <div className="newsfeed__chart">
             <LineGraph />
+            <TimeLine />
+          </div>
+        </div>
+        <div className="newsfeed__buying__section">
+          <h2> Buying Power</h2>
+          <h2> $4.11</h2>
+        </div>
+        <div className="newsfeed__market__section">
+          <div className="newsfeed__market__box">
+            <p> Markets Closed</p>
+            <h1> Happy Thanksgiving</h1>
+          </div>
+        </div>
+        <div className="newsfeed__popularlists__section">
+          <div className="newsfeed__popularlists__intro">
+            <h1>Popular lists</h1>
+            <p>Show More</p>
+          </div>
+          <div className="newsfeed_popularlists_badges">
+            {popularTopics.map((topic) => (
+              <Chip
+                className="topic__badge"
+                variant="outlined"
+                label={topic}
+                avatar={
+                  <Avatar
+                    src={`https://avatars.dicebear.com/api/human/${topic}.svg`}
+                  />
+                }
+              />
+            ))}
           </div>
         </div>
       </div>
